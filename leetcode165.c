aa40badf74fe2e4d9ebbe374143e411acdefeb81
李晨龙 <656233622@qq.com>
2016-12-30 13:49:23 +0800

    Update leetcode165.c

--- leetcode165.c
+++ leetcode165.c
@@ -1,77 +1,30 @@
-
-int num=0;
-
-char* moveStr(char* s){
-	while( (*s != '\0') && (*s != '.') )
-	{
-		s++;
-	}
-
-	if(*s == '\0')
-	{
-		return NULL;
-	}
-	else
-	{
-		s++;
-		return s;
-	}
-}
-
-
-char* nextNumToGlobalNum(char* s)
-{
-    if(s==NULL)
-    {
-        return NULL;
-    }
-    
-	s=moveStr(s);
-	if(s==NULL)
-	{
-		num=0;
-		return NULL;
-	}
-	else
-	{
-		num=atoi(s);
-		return s;
-	}
-
-}
-
-
-
 int compareVersion(char* version1, char* version2) {
-	/*int v1Len=strlen(version1);
-	int v2Len=strlen(version2);*/
-	int v1=0;
-	int v2=0;
+	int v1=0,v2=0;
+	char *old_version1=NULL, *old_version2=NULL;
+	char *context1=NULL, *context2=NULL;
 
-	v1=atoi(version1);
-	v2=atoi(version2);
-	if(v1!=v2) 
-	{
-		return (v1>v2)?(1):(-1);
-	}
+	while(1) {
+		old_version1=strtok_r(version1,".",&context1);
+		(old_version1!=NULL)?(v1=atoi(old_version1)):(v1=0);
 
-	while(1)
-	{
-	    num=0;
-	    version1=nextNumToGlobalNum(version1); v1=num;
-		
-		num=0;
-		version2=nextNumToGlobalNum(version2); v2=num;
-		
-		if(v1==v2 && nextNumToGlobalNum(version1)==NULL && nextNumToGlobalNum(version2)==NULL)
+		old_version2=strtok_r(version2,".",&context2);
+		(old_version2!=NULL)?(v2=atoi(old_version2)):(v2=0);
+
+		if(v1==v2) 
 		{
-			return 0;
+			if(old_version1==NULL)
+			{
+				if(old_version2==NULL)
+				{
+					return 0;
+				}
+			}
 		}
 		
-		if(v1!=v2)
-		{
+		version1=version2=NULL;
+
+		if(v1!=v2) {
 			return (v1>v2)?(1):(-1);
 		}
-
 	}
 }
